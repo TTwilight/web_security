7e1b357320e92605ef2d2a7bbde9b20cb7883d6a
TTwilight <ttwilight2016@gmail.com>
2017-01-04 20:14:23 +0800

    v0.5 优化了地图查询,增加了首页显示，优化了显示细节，添加了导航兰

--- web_security/web_security/urls.py
+++ web_security/web_security/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
+from . import views
 
 urlpatterns = [
+    url(r'^$',views.home,name='home'),
     url(r'^admin/', admin.site.urls),
     url(r'^encrypt/',include('encrypt.urls')),
     url(r'^ipsearch/',include('ipsearch.urls')),
+
 ]
---
+++ web_security/web_security/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse,Http404
+from urllib.request import urlopen,urlretrieve
+from bs4 import BeautifulSoup
+import json
+import os
+from binascii import a2b_hex,b2a_hex
+
+def home(request):
+    return render(request,'home.html')
