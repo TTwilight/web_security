3119c17dfdf27eabbedf254a5e3701dd9777d676
TTwilight <ttwilight2016@gmail.com>
2017-01-04 19:02:58 +0800

    v0.5 优化了地图查询

--- web_security/ipsearch/views.py
+++ web_security/ipsearch/views.py
@@ -10,15 +10,22 @@ from binascii import a2b_hex,b2a_hex
 def index(request):
     ipaddress=request.POST.get('ipadd')
     b=isinstance(ipaddress,str)
-    if os.path.exists('/var/python_test/test/django/web_security/web_security/ipsearch/static/ipsearch/test.jpg'):
-        os.remove('/var/python_test/test/django/web_security/web_security/ipsearch/static/ipsearch/test.jpg')
+    if os.path.exists(os.path.join(os.path.abspath('.')+'/ipsearch/static/ipsearch/test.jpg')):
+        os.remove(os.path.join(os.path.abspath('.')+'/ipsearch/static/ipsearch/test.jpg'))
     if ipaddress:
         a='http://freegeoip.net/json/'+ipaddress
         html=urlopen(a)
         hjson=json.loads(html.read().decode('utf-8'))
         longitude=str(hjson['longitude'])
         latitude=str(hjson['latitude'])
+        country=str(hjson['country_name'])
+        address=str(hjson['region_name'])+' '+str(hjson['city'])
+        time_zone=str(hjson['time_zone'])
         baiduapi='http://api.map.baidu.com/staticimage/v2?ak=hNFf8i21Y8lPPlNbMXvt247DCIrtbDIR&center='+longitude+','+latitude+'&width=400&height=400&zoom=12'
-        path='/var/python_test/test/django/web_security/web_security/ipsearch/static/ipsearch/'+'test.jpg'
+        path=os.path.join(os.path.abspath('.')+'/ipsearch/static/ipsearch/')+'test.jpg'     #部署注意修改文件权限，不然不能访问，chmod 777 ipsearch/*
         urlretrieve(baiduapi,path)
-    return render(request,'ipsearch/index.html',{'ipadd':ipaddress})
+    else:
+        country=''
+        address=''
+        time_zone=''
+    return render(request,'ipsearch/index.html',{'ipadd' :ipaddress,'country':country,'address':address,'time_zone':time_zone})
