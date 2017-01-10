85bba7833ed15fb4781c86b8c9b41429004b916a
TTwilight <ttwilight2016@gmail.com>
2017-01-10 17:54:25 +0800

    v0.62 优化目录

--- ipsearch/views.py
+++ ipsearch/views.py
@@ -10,8 +10,8 @@ from binascii import a2b_hex,b2a_hex
 def index(request):
     ipaddress=request.POST.get('ipadd')
     b=isinstance(ipaddress,str)
-    if os.path.exists(os.path.join(os.path.abspath('.')+'/ipsearch/static/ipsearch/test.jpg')):
-        os.remove(os.path.join(os.path.abspath('.')+'/ipsearch/static/ipsearch/test.jpg'))
+    if os.path.exists(os.path.join(os.path.abspath('.'),'ipsearch/static/ipsearch/test.jpg')):
+        os.remove(os.path.join(os.path.abspath('.'),'ipsearch/static/ipsearch/test.jpg'))
     if ipaddress:
         a='http://freegeoip.net/json/'+ipaddress
         html=urlopen(a)
@@ -22,7 +22,7 @@ def index(request):
         address=str(hjson['region_name'])+' '+str(hjson['city'])
         time_zone=str(hjson['time_zone'])
         baiduapi='http://api.map.baidu.com/staticimage/v2?ak=hNFf8i21Y8lPPlNbMXvt247DCIrtbDIR&center='+longitude+','+latitude+'&width=400&height=400&zoom=12'
-        path=os.path.join(os.path.abspath('.')+'/ipsearch/static/ipsearch/')+'test.jpg'     #部署注意修改文件权限，不然不能访问，chmod 777 ipsearch/*
+        path=os.path.join(os.path.abspath('.'),'ipsearch/static/ipsearch/')+'test.jpg'     #部署注意修改文件权限，不然不能访问，chmod 777 ipsearch/*
         urlretrieve(baiduapi,path)
         ports=showport(ipaddress)
     else:
