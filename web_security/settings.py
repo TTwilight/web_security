1fef8818210c2b85c1cbf3507ff555aba83f8b79
TTwilight <ttwilight2016@gmail.com>
2017-01-10 17:32:20 +0800

    v0.62 优化目录

--- web_security/settings.py
+++ web_security/settings.py
@@ -122,7 +122,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-
 STATIC_URL = '/static/'
+STATIC_ROOT= os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'ipsearch/static'),
+)
 
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATICFILES_FINDERS = (
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+)
