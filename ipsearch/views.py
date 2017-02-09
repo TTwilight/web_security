f0a9ccfaf5ae2c90a327a715dfc90c89ca85a3b1
TTwilight <ttwilight2016@gmail.com>
2017-02-09 20:09:13 +0800

    v1.0 优化了目录结构,完善了功能,优化了细节

--- ipsearch/views.py
+++ ipsearch/views.py
@@ -9,22 +9,30 @@ from binascii import a2b_hex,b2a_hex
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 def index(request):
     ipaddress=request.POST.get('ipadd')
-    b=isinstance(ipaddress,str)
+
     if os.path.exists(os.path.join(BASE_DIR,'ipsearch/static/ipsearch/test.jpg')):    #apache 中会找不到app的静态文件，需要使用python manage.py collectstatic 将其收集到根目录static文件下,
         os.remove(os.path.join(BASE_DIR,'ipsearch/static/ipsearch/test.jpg'))         #然后配置/etc/apache2/sites-available里conf 文件,另外需要将此出以及path处的ipsearch/static/ipsearch/test.jpg
                                                                                       #换成  static/ipsearch/test.jpg
     if ipaddress:
-        a='http://freegeoip.net/json/'+ipaddress
-        html=urlopen(a)
-        hjson=json.loads(html.read().decode('utf-8'))
-        longitude=str(hjson['longitude'])
-        latitude=str(hjson['latitude'])
-        country=str(hjson['country_name'])
-        address=str(hjson['region_name'])+' '+str(hjson['city'])
-        time_zone=str(hjson['time_zone'])
-        baiduapi='http://api.map.baidu.com/staticimage/v2?ak=hNFf8i21Y8lPPlNbMXvt247DCIrtbDIR&center='+longitude+','+latitude+'&width=400&height=400&zoom=12'
-        path=os.path.join(BASE_DIR,'ipsearch/static/ipsearch/')+'test.jpg'     #部署注意修改文件权限，不然不能访问，chmod 777 ipsearch/*
-        urlretrieve(baiduapi,path)
+        ipaddress=ipaddress.strip()
+        try:
+            a='http://freegeoip.net/json/'+ipaddress
+            html=urlopen(a)
+            hjson=json.loads(html.read().decode('utf-8'))
+            longitude=str(hjson['longitude'])
+            latitude=str(hjson['latitude'])
+            country=str(hjson['country_name'])
+            address=str(hjson['region_name'])+' '+str(hjson['city'])
+            time_zone=str(hjson['time_zone'])
+            baiduapi='http://api.map.baidu.com/staticimage/v2?ak=hNFf8i21Y8lPPlNbMXvt247DCIrtbDIR&center='+longitude+','+latitude+'&width=400&height=400&zoom=12'
+            path=os.path.join(BASE_DIR,'ipsearch/static/ipsearch/')+'test.jpg'     #部署注意修改文件权限，不然不能访问，chmod 777 ipsearch/*
+            urlretrieve(baiduapi,path)
+        except Exception as e:
+            country = ''
+            address = ''
+            time_zone = ''
+            return render(request, 'ipsearch/index.html',
+                          {'ipadd': ipaddress, 'country': country, 'address': address, 'time_zone': time_zone, 'error':e})
     else:
         country=''
         address=''
--- scanner/views.py
+++ scanner/views.py
@@ -2,13 +2,21 @@ from django.shortcuts import render
 
 # Create your views here.
 from urllib.request import Request,urlopen
-import nmap
+import nmap,socket
 
 def index(request):
     host=request.POST.get('host_name')
     if host:
-        tcps=showport(host)
-        return render(request,'scanner/index.html',{'tcps':tcps,'host':host,})
+        hostname = host.strip()
+        host=host.strip()
+        try:
+            host = socket.gethostbyname(host)
+            tcps=showport(host)
+            return render(request,'scanner/index.html',{'tcps':tcps,'host':hostname,})
+        except Exception as e:
+            tcps = ['0']
+
+            return render(request, 'scanner/index.html', {'tcps': tcps,'error':e })
     else:
         tcps=['0']
         return render(request, 'scanner/index.html', {'tcps': tcps, })
@@ -16,7 +24,7 @@ def showport(host):
     host=str(host)
     ports = []
     nm=nmap.PortScanner()
-    result=nm.scan(host,'0-8000')
+    result=nm.scan(host,'0-10000')
     if result['scan']:
         tcps=result['scan'][host]['tcp']
         return tcps
